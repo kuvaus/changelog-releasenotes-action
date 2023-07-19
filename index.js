647a8f2afadfb6803bbd7b6bc10208319bb23602
kuvaus <kuvaus@users.noreply.github.com>
2023-07-19 04:55:09 +0300

    Add ability to update release

--- index.js
+++ index.js
@@ -12,19 +12,25 @@ async function main() {
     // Parse the changelog
     let filtered_lines = [];
     let start_processing = false;
+    let index = 0;
+    let tagName;
 
     const changelogPath = path.join(process.env.GITHUB_WORKSPACE, 'CHANGELOG.md');
     const lines = fs.readFileSync(changelogPath, 'utf-8').split('\n');
     for(let line of lines) {
+      index += 1;
       if(line.startsWith("#### [v")) {
         if(start_processing) {
           break;
         } else {
+          const version_tag = line.match(/v[\d\.]+/);
+          tagName = version_tag[0];
           start_processing = true;
+          index = 0;
           continue;
         }
       }
-      if(start_processing) {
+      if(start_processing && index >= 2) {
         filtered_lines.push(line);
       }
     }
@@ -39,21 +45,51 @@ async function main() {
     const { owner, repo } = github.context.repo;
     //const { ref_name: tagName } = github.context.ref;
     const ref = github.context.ref;
-    const refParts = ref.split('/');
-    const tagName = refParts[refParts.length - 1];
+    //const refParts = ref.split('/');
+    //const tagName = refParts[refParts.length - 1];
+    console.log(tagName);
+    // Check if ref is a tag
+    if(ref.startsWith('refs/tags/')) {
+      const refParts = ref.split('/');
+      tagName = refParts[refParts.length - 1];
+    }
+    
+    
+    console.log(github.context.ref);
+    console.log(refParts);
+    console.log(tagName);
 
 
 
-    const response = await octokit.rest.repos.createRelease({
+    // Fetch all releases
+    const releases = await octokit.rest.repos.listReleases({
       owner,
       repo,
-      tag_name: tagName,
-      name: `Release ${tagName}`,
-      body: releaseBody,
-      draft: false,
-      prerelease: false,
     });
-
+    
+    // Check if your release exists
+    let release = releases.data.find(r => r.tag_name === tagName);
+    
+    // If release exists, update it
+    if(release) {
+      const updatedRelease = await octokit.rest.repos.updateRelease({
+        owner,
+        repo,
+        release_id: release.id,
+        body: releaseBody,
+      });
+    } else {
+      // If release does not exist, create it
+      const response = await octokit.rest.repos.createRelease({
+        owner,
+        repo,
+        tag_name: tagName,
+        name: `Release ${tagName}`,
+        body: releaseBody,
+        draft: false,
+        prerelease: false,
+      });
+    }
 
   } catch (error) {
     core.setFailed(error.message);
