58ac0946b63780d461374246b583bd7b7341b8f0
kuvaus <kuvaus@users.noreply.github.com>
2023-07-22 19:33:19 +0300

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const fetch = require('node-fetch').default;
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
@@ -94,7 +94,8 @@ async function read_filtered_changelog(options) {
 async function create_release(release_notes, options) {
 
     const token = process.env.GITHUB_TOKEN; 
-    const octokit = new Octokit({ auth: token });
+    //const octokit = new Octokit({ auth: token });
+    const octokit = new Octokit({ auth: token, request: {fetch: fetch, } });
     
     // Create a new release
     const { owner, repo } = github.context.repo;
